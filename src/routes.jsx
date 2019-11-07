0679e1ebe706079253683f093dd9568303dd2d0b
Sebastian Serrano <saserrano@falabella.cl>
2019-11-07 00:32:07 -0300

    Login and register page, alert component

--- src/routes.jsx
+++ src/routes.jsx
@@ -2,13 +2,13 @@ import HomePage from './containers/HomePage';
 import DetailPage from './containers/DetailPage';
 import PaymentPage from './containers/PaymentPage';
 import OrderListPage from './containers/OrderListPage';
+import AuthPage from './containers/AuthPage';
 
 export default [
   { exact: true, path: '/', component: HomePage },
   { exact: true, path: '/home', component: HomePage },
   { exact: true, path: '/detail/:id', component: DetailPage },
   { exact: true, path: '/payment', component: PaymentPage },
-  { exact: true, path: '/order-list', component: OrderListPage }
-  // { exact: true, path: '/sign-in', component: OrderListPage },
-  // { exact: true, path: '/sign-up', component: OrderListPage }
+  { exact: true, path: '/order-list', component: OrderListPage },
+  { exact: true, path: '/auth', component: AuthPage }
 ];
